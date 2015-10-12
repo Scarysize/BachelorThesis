ac748cfa4f884b84dcb9b935ef53581c6d68bf01
Scarysize <znarf92@gmx.net>
2015-10-12 12:59:31 +0200

    played around with delaunay3d triangulation

--- gridReader/gridReader/main.cpp
+++ gridReader/gridReader/main.cpp
@@ -18,22 +18,66 @@
 #include <vtkUnstructuredGridReader.h>
 #include <vtkUnstructuredGridGeometryFilter.h>
 
+#include <vtkXMLUnstructuredGridWriter.h>
+
 #include <vtkGenericDataObjectReader.h>
 #include <vtkDataSet.h>
 #include <vtkDataSetMapper.h>
 
 #include <vtkPolyData.h>
+#include <vtkCleanPolyData.h>
 #include <vtkPolyDataMapper.h>
+
+#include <vtkDelaunay3D.h>
+
+#include <vtkGeometryFilter.h>
+
+#include <vtkQuadricDecimation.h>
+#include <vtkDecimatePro.h>
 
 #include <vtkFieldDataToAttributeDataFilter.h>
 
+#include <vtkActor.h>
+#include <vtkProperty.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 
-void visualize(vtkSmartPointer<vtkPolyDataMapper> mapper) {
+
+vtkSmartPointer<vtkActor> delaunay3d(std::string filename) {
+    //Read .vtk file
+    vtkSmartPointer<vtkUnstructuredGridReader> gridReader = vtkSmartPointer<vtkUnstructuredGridReader>::New();
+    gridReader->SetFileName(filename.c_str());
+    gridReader->Update();
     
- 
+    vtkSmartPointer<vtkGeometryFilter> geoFilter = vtkSmartPointer<vtkGeometryFilter>::New();
+    geoFilter->SetInputData(gridReader->GetOutput());
+    geoFilter->Update();
+    vtkPolyData *gridToPoly = geoFilter->GetOutput();
+    
+    //Get clean poly data --> removes duplicates
+    vtkSmartPointer<vtkCleanPolyData> cleanpoly = vtkSmartPointer<vtkCleanPolyData>::New();
+    //cleanpoly->SetInputData(gridToPoly);
+    
+    //Generate 3d triangle mesh
+    vtkSmartPointer<vtkDelaunay3D> delaunay3D = vtkSmartPointer<vtkDelaunay3D>::New();
+    delaunay3D->SetInputData(gridToPoly);
+    
+    //WriteDataSet(delaunay3D->GetOutput(), "/Volumes/EXTERN/Bachelor Arbeit/delaunay3d.vtp");
+    
+    vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer = vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
+    writer->SetInputConnection(delaunay3D->GetOutputPort());
+    writer->SetFileName("/Volumes/EXTERN/delaunay3d.vtu");
+    writer->Write();
+    
+    vtkSmartPointer<vtkDataSetMapper> delaunayMapper = vtkSmartPointer<vtkDataSetMapper>::New();
+    delaunayMapper->SetInputConnection(delaunay3D->GetOutputPort());
+    
+    vtkSmartPointer<vtkActor> delaunayActor = vtkSmartPointer<vtkActor>::New();
+    delaunayActor->SetMapper(delaunayMapper);
+    delaunayActor->GetProperty()->SetColor(1,0,0);
+    
+    return delaunayActor;
 }
 
 int main(int argc, const char * argv[]) {
@@ -78,12 +122,40 @@ int main(int argc, const char * argv[]) {
     } else if (reader->IsFileUnstructuredGrid()) {
         std::cout << "ouput is unstructured grid" << std::endl;
         
+        vtkSmartPointer<vtkActor> delaunay = delaunay3d(inputFilename);
+        
         //Initialize UnstructuredGridReader
         vtkSmartPointer<vtkUnstructuredGridReader> gridReader = vtkSmartPointer<vtkUnstructuredGridReader>::New();
         gridReader->SetFileName(inputFilename.c_str());
         gridReader->Update();
         
-        vtkUnstructuredGrid *ugrid = gridReader->GetOutput();
+        vtkSmartPointer<vtkUnstructuredGrid> ugrid = gridReader->GetOutput();
+        std::cout << "cell type 0; " << ugrid->GetCellType(0) << std::endl;
+        std::cout << "cell type 100; " << ugrid->GetCellType(100) << std::endl;
+        std::cout << "cell type 1000; " << ugrid->GetCellType(1000) << std::endl;
+        std::cout << "cell type 6000; " << ugrid->GetCellType(6000) << std::endl;
+        
+        
+        //Convert grid to PolyData for decimation
+        /*vtkSmartPointer<vtkGeometryFilter> geoFilter = vtkSmartPointer<vtkGeometryFilter>::New();
+        geoFilter->SetInputData(ugrid);
+        geoFilter->Update();
+        vtkPolyData *poly = geoFilter->GetOutput();
+        
+        std::cout << "polydata cells:  " << poly->GetNumberOfCells() << std::endl;
+        std::cout << "polydata points: " << poly->GetNumberOfPoints() << std::endl;
+        
+        //Decimation
+        vtkSmartPointer<vtkDecimatePro> decimate = vtkSmartPointer<vtkDecimatePro>::New();
+        decimate->SetInputData(poly);
+        decimate->SetTargetReduction(.1);
+        decimate->Update();
+        
+        vtkSmartPointer<vtkPolyData> decimated = vtkSmartPointer<vtkPolyData>::New();
+        decimated->ShallowCopy(decimate->GetOutput());
+        
+        std::cout << "decimated number of cells: " << decimated->GetNumberOfCells() << std::endl;*/
+        
         
         //Field data --> to Cells
         vtkSmartPointer<vtkFieldDataToAttributeDataFilter> toCellFilter = vtkSmartPointer<vtkFieldDataToAttributeDataFilter>::New();
@@ -106,7 +178,7 @@ int main(int argc, const char * argv[]) {
         
         //Renderer
         vtkSmartPointer<vtkRenderer> ugridRenderer = vtkSmartPointer<vtkRenderer>::New();
-        ugridRenderer->AddActor(ugridActor);
+        ugridRenderer->AddActor(delaunay);
         ugridRenderer->SetBackground(0.1, 0.2, 0.4);
         
         //Render Window & Interactor
@@ -118,7 +190,7 @@ int main(int argc, const char * argv[]) {
         iren->Initialize();
         iren->Start();
         
-        std::cout << "number of cells: " << ugrid->GetNumberOfCells() << std::endl;
+        //std::cout << "number of cells: " << ugrid->GetNumberOfCells() << std::endl;
     } else if (reader->IsFileStructuredGrid()) {
         std::cout << "output is structured grid" << std::endl;
     }
